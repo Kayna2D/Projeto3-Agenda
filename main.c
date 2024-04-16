8f057e18b43dbf6382acdb586c16fd466dadf6c1
Kayna2d <65991310+Kayna2D@users.noreply.github.com>
2024-04-16 22:56:22 +0000

    printa todas as opcoes em um loop infinito

--- main.c
+++ main.c
@@ -1,5 +1,13 @@
-#include stdio.h
+#include <stdio.h>
 
-main() {
-  
+int main() {
+  int opcao = 1;
+  do {
+    printf("\nMenu principal\n");
+    printf("1 - Criar tarefa\n");
+    printf("2 - Deletar tarefa\n");
+    printf("3 - Listar tarefas\n");
+    printf("0 - Sair\n");
+    printf("Entre com uma opção: ");
+  } while (opcao != 0);    
 }

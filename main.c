62d154ff13266cced8b0d4c3340295f556c5d64d
Kayna2d <65991310+Kayna2D@users.noreply.github.com>
2024-04-16 22:58:31 +0000

    recebe opcao digitada

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-  int opcao = 1;
+  int opcao;
   do {
     printf("\nMenu principal\n");
     printf("1 - Criar tarefa\n");
@@ -9,5 +9,7 @@ int main() {
     printf("3 - Listar tarefas\n");
     printf("0 - Sair\n");
     printf("Entre com uma opção: ");
+    int i = scanf("%d", &opcao);
+     printf("Opção escolhida: %d\n", opcao);
   } while (opcao != 0);    
 }
